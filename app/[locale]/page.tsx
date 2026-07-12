a837c6b904c22a8faabca6aee86b71919dd328c1
Lucas Andrade <lafdesouza2002@outlook.com>
2026-07-12 16:38:06 -0300

    feat(videos): add home-page latest-videos teaser
    VideosTeaser renders nothing until videosData has 3+ entries, then
    shows the 3 newest as VideoCards with a "latest videos" heading and an
    "all videos" link to /videos, inserted between ExperienceSummary and
    HomeLayout.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import FeaturedLayout from '@/layouts/FeaturedLayout'
 import HomeLayout from '@/layouts/HomeLayout'
 import HeroSection from '@/components/home/HeroSection'
 import ExperienceSummary from '@/components/home/ExperienceSummary'
+import VideosTeaser from '@/components/home/VideosTeaser'
 import { LocaleTypes } from './i18n/settings'
 
 interface PageProps {
@@ -27,6 +28,7 @@ export default async function Page({ params }: PageProps) {
         <FeaturedLayout posts={hasFeaturedPosts} params={{ locale }} />
       )}
       <ExperienceSummary />
+      <VideosTeaser locale={locale} />
       <HomeLayout posts={filteredPosts} params={{ locale }} />
     </>
   )
